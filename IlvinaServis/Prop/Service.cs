1948959c2c6484e5cb3a3abaa400f97a5e5922c0
Шайхутдинова Ильвина Ильшатовна <201719@mck-ktits.ru>
2021-02-18 18:56:59 +0300

    viewService

---
+++ IlvinaServis/Prop/Service.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IlvinaServis
+{
+    partial class Service
+    {
+        public int GetDurationInSeconds
+        {
+            get
+            {
+                return DurationInSeconds= DurationInSeconds / 60;
+            }
+        }
+        public decimal GetCost
+        {
+            get
+            {
+                return Math.Round(Cost,0);
+            }
+        }
+        public double GetDiscount
+        {
+            get
+            {
+                double dis = Convert.ToDouble(GetDiscount/100);
+                double cost = Convert.ToDouble(GetCost);
+                double d =  +cost*dis;
+                return d;
+            }
+        }
+    }
+}
